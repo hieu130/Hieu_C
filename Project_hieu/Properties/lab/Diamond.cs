429663be9aa58b847d91d41be37569e86d24d0f2
mylaptop <hieuvanphan275@gmail.com>
2021-06-08 11:06:34 +0700

    lab s2

---
+++ Project_hieu/Properties/lab/Diamond.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Project_hieu.Properties.lab
+{
+    public class Diamond : Product
+    {
+        public int cara;
+
+        public Diamond()
+        {
+        }
+
+        public Diamond(int cara)
+        {
+            this.cara = cara;
+        }
+
+        public Diamond(int id, string name, float price, int qty, int image, int desc, int cara) : base(id, name, price, qty, image, desc)
+        {
+            this.cara = cara;
+        }
+
+        public void Test()
+        {
+            if(cara > 1.7)
+            {
+                Console.WriteLine("Real");
+            }
+            else
+            {
+                Console.WriteLine("Fake");
+            }
+        }
+        
+    }
+}
---
+++ Project_hieu/Properties/lab/Fashion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Project_hieu.Properties.lab
+{
+    public class Fashion : Product
+    {
+        public string color;
+        public string size;
+
+        public Fashion()
+        {
+        }
+
+        public Fashion(string color, string size)
+        {
+            this.color = color;
+            this.size = size;
+        }
+
+        public Fashion(int id, string name, float price, int qty, int image, int desc, string color, string size) :
+            base(id, name, price, qty, image, desc)
+        {
+            this.color = color;
+            this.size = size;
+        }
+
+        public void checkProduct()
+        {
+        }
+
+        public void checkQty()
+        {
+            if (qty > 0)
+            {
+                Console.WriteLine("San Pham Con Hang");
+            }
+            else
+            {
+                Console.WriteLine("San Pham Het Hang");
+            }
+        }
+
+        public void checkInfoPro()
+        {
+            if (color == "Xanh" && size == "L")
+            {
+                Console.WriteLine("San Pham Co Trong Kho");
+            }
+
+            Console.WriteLine("San Pham Khong Co Trong Kho");
+
+        }
+    }
+}
